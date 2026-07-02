32448b8f522b7065c0762b51b29ee52439ad8a29
Your Name <your-email@example.com>
2026-07-02 11:47:59 +0400

    Additional fixes for hangman

---
+++ Minigames.Persistence/Migrations/20260702063023_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minigames.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GameSummary_HangmanGameResult_TimesPlayed = table.Column<int>(type: "int", nullable: false),
+                    GameSummary_HangmanGameResult_TimesWon = table.Column<int>(type: "int", nullable: false),
+                    GameSummary_FormulaGameResult_TimesPlayed = table.Column<int>(type: "int", nullable: false),
+                    GameSummary_FormulaGameResult_BestDifference = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
